5ea8b1725e12c845b0178c512c5a3e4363687654
OmerGulcu <gulcuomer@gmail.com>
2023-01-31 12:54:13 +0300

    Finish lecture

--- Week 5/src5/counter4.js
+++ Week 5/src5/counter4.js
@@ -1,15 +1,17 @@
-let counter = 0;
+if (!localStorage.getItem('counter')) {
+    localStorage.setItem('counter', 0);
+}
 
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('button').onclick = count;
-});
+let counter = localStorage.getItem('counter');
 
 function count() {
     counter++;
-
-    if (counter % 10 === 0) {
-        alert(`Count is now ${counter}`);
-    }
-
     document.querySelector('h1').innerHTML = counter;
+    localStorage.setItem('counter', counter);
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('button').onclick = count;
+    document.querySelector('h1').innerHTML = localStorage.getItem('counter');
+});
+
